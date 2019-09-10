8da3a40480f4f602ca3393923699d28a79444aac
petercolella <peter.joseph.colella@gmail.com>
2019-09-09 20:56:22 -0400

    adds form to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import logo from './logo.svg';
 import './App.css';
+import API from './utils/API';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const populate = () => {
+    API.populateAssignments({ email, password }).then(() => {
+      setEmail('');
+      setPassword('');
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="email"
+            label="Email"
+            className={classes.textField}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            margin="normal"
+          />
+          <TextField
+            id="password"
+            label="Password"
+            className={classes.textField}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            margin="normal"
+          />
+        </form>
+        <Button variant="contained" color="primary" onClick={populate}>
+          Submit
+        </Button>
       </header>
     </div>
   );
