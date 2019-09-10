4d78f151f0c09a352db6f408c03e528b4587611d
petercolella <peter.joseph.colella@gmail.com>
2019-09-09 20:55:56 -0400

    adds API util to client

---
+++ client/src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export default {
+  populateAssignments: function(reqBody) {
+    return axios.post('/api/assignments/populate', reqBody);
+  },
+  getAssignments: function() {
+    return axios.get('/api/assignments');
+  },
+  getAssignment: function(id) {
+    return axios.get('/api/assignments/' + id);
+  },
+  deleteAssignment: function(id) {
+    return axios.delete('/api/assignments/' + id);
+  },
+  saveAssignment: function(assignmentData) {
+    return axios.post('/api/assignments', assignmentData);
+  },
+  updateAssignment: function(id, assignmentData) {
+    return axios.put('/api/assignments/' + id, assignmentData);
+  }
+};
