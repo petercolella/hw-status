915e024bc7d5d05728f5cceeb5263a392abd58c6
petercolella <peter.joseph.colella@gmail.com>
2019-09-13 22:26:33 -0400

    adds new functions for separation of concerns in assignmentsController.js

--- controllers/assignmentsController.js
+++ controllers/assignmentsController.js
@@ -1,6 +1,63 @@
 const db = require('../models');
 const axios = require('axios');
 
+function axiosConfig(response, courseId) {
+  return {
+    url: 'https://www.bootcampspot.com/api/instructor/v1/grades',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      authToken: response.data.authenticationInfo.authToken
+    },
+    data: { courseId: parseInt(courseId) }
+  };
+}
+
+function createAssignmentIdArray(assignments) {
+  return new Promise((resolve, reject) => {
+    const idArr = [];
+    assignments.forEach(doc => idArr.push(doc._id));
+    resolve(idArr);
+    reject('Something went wrong while creating the assignment ID array.');
+  });
+}
+
+function findCourse(courseId) {
+  return db.Course.findOne({ courseId: parseInt(courseId) });
+}
+
+function newCourse(courseId, idArr) {
+  return db.Course.create({
+    courseId: parseInt(courseId),
+    assignments: idArr
+  });
+}
+
+function deleteCourseAssignments(docs) {
+  return new Promise((resolve, reject) => {
+    const promises = [];
+    for (let assignmentId of docs.assignments) {
+      promises.push(db.Assignment.findByIdAndDelete(assignmentId));
+    }
+    Promise.all(promises)
+      .then(arrayOfDeletedAssignments => resolve(arrayOfDeletedAssignments))
+      .catch(err => reject(err));
+  });
+}
+
+function updateCourse(docs, idArr) {
+  return db.Course.findOneAndUpdate(
+    { _id: docs._id },
+    { $set: { assignments: idArr } },
+    { new: true }
+  );
+}
+
+function handleError(res, err) {
+  console.error(err.message);
+  return res.status(err.response.status).json(err);
+}
+
 module.exports = {
   populate: function(req, res) {
     const { email, password, courseId } = req.body;
@@ -8,58 +65,40 @@ module.exports = {
     axios
       .post('https://www.bootcampspot.com/api/instructor/v1/login', login)
       .then(response => {
-        const settings = {
-          url: 'https://www.bootcampspot.com/api/instructor/v1/grades',
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            authToken: response.data.authenticationInfo.authToken
-          },
-          data: { courseId: parseInt(courseId) }
-        };
-        axios(settings)
+        axios(axiosConfig(response, courseId))
           .then(response => {
-            db.Assignment.insertMany(response.data, function(err, assignments) {
-              if (err) return console.log('Insert Error:', err);
-              const idArr = [];
-              assignments.forEach(doc => idArr.push(doc._id));
-              db.Course.findOne(
-                { courseId: parseInt(courseId) },
-                (err, docs) => {
-                  if (err) console.error(err);
-                  if (!docs) {
-                    db.Course.create(
-                      { courseId: parseInt(courseId), assignments: idArr },
-                      { new: true },
-                      (err, dbModel1) => {
-                        if (err) console.error(err);
-                        console.log('dbModel1', dbModel1);
-                        res.json(dbModel1);
-                      }
-                    );
-                  } else {
-                    docs.assignments.forEach(assignmentId => {
-                      db.Assignment.findByIdAndDelete(assignmentId, err => {
-                        if (err) console.error(err);
-                      });
-                    });
-                    db.Course.findOneAndUpdate(
-                      { courseId: parseInt(courseId) },
-                      { $set: { assignments: idArr } },
-                      { new: true },
-                      (err, updatedCourse2) => {
-                        if (err) console.error(err);
-                        console.log('updatedCourse2', updatedCourse2);
-                        res.json(updatedCourse2);
-                      }
-                    );
-                  }
-                }
-              );
-            });
+            db.Assignment.insertMany(response.data)
+              .then(assignments => createAssignmentIdArray(assignments))
+              .then(idArr => {
+                findCourse(courseId)
+                  .then(docs => {
+                    if (!docs) {
+                      newCourse(courseId, idArr)
+                        .then(newCourse => {
+                          console.log('newCourse:', newCourse);
+                          res.json(newCourse);
+                        })
+                        .catch(err => res.status(422).json(err));
+                    } else {
+                      deleteCourseAssignments(docs)
+                        .then(() => {
+                          updateCourse(docs, idArr)
+                            .then(updatedCourse => {
+                              console.log('updatedCourse:', updatedCourse);
+                              res.json(updatedCourse);
+                            })
+                            .catch(err => res.status(422).json(err));
+                        })
+                        .catch(err => handleError(res, err));
+                    }
+                  })
+                  .catch(err => handleError(res, err));
+              })
+              .catch(err => handleError(res, err));
           })
-          .catch(err => console.log(err.message));
-      });
+          .catch(err => handleError(res, err));
+      })
+      .catch(err => handleError(res, err));
   },
   findAll: function(req, res) {
     db.Assignment.find(req.query)
