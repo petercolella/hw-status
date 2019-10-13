5aa4de9239046198a9d705f9d0aa7da226d8fd9d
petercolella <peter.joseph.colella@gmail.com>
2019-10-13 17:31:44 -0400

    adds Current Course ID typography to CurrentCourse component

--- client/src/components/CurrentCourse/index.js
+++ client/src/components/CurrentCourse/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper } from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
 import InactiveStudents from '../InactiveStudents';
 import FilteredAssignments from '../FilteredAssignments';
 
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(0, 1, 1)
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  },
+  headingContainer: {
+    display: 'flex',
+    fontSize: 16,
+    justifyContent: 'flex-start',
+    margin: theme.spacing(0, 1)
   },
   paper: {
     margin: theme.spacing(2),
@@ -19,18 +29,26 @@ const CurrentCourse = props => {
 
   return (
     <Paper className={classes.paper} elevation={4}>
-      <InactiveStudents
-        assignments={props.assignments}
-        courseDbId={props.courseDbId}
-        inactiveStudents={props.inactiveStudents}
-        loadData={props.loadData}
-      />
-      <FilteredAssignments
-        assignments={props.assignments}
-        courseDbId={props.courseDbId}
-        filteredAssignments={props.filteredAssignments}
-        loadData={props.loadData}
-      />
+      <Typography
+        className={classes.headingContainer}
+        variant="h6"
+        gutterBottom>
+        Current Course ID: {props.currentCourseId}
+      </Typography>
+      <div className={classes.buttonContainer}>
+        <InactiveStudents
+          assignments={props.assignments}
+          courseDbId={props.courseDbId}
+          inactiveStudents={props.inactiveStudents}
+          loadData={props.loadData}
+        />
+        <FilteredAssignments
+          assignments={props.assignments}
+          courseDbId={props.courseDbId}
+          filteredAssignments={props.filteredAssignments}
+          loadData={props.loadData}
+        />
+      </div>
     </Paper>
   );
 };
