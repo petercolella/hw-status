24c21285e740f71f2c74dc576168e6a05d3382d2
petercolella <peter.joseph.colella@gmail.com>
2019-10-13 16:10:51 -0400

    adds Grid component to Form component & updates styling

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap'
+    margin: theme.spacing(0, 1)
   },
   textField: {
-    margin: theme.spacing(2, 2, 0),
-    width: 200
+    width: '100%'
   }
 }));
 
@@ -17,41 +16,53 @@ const Form = props => {
   const classes = useStyles();
   return (
     <form className={classes.container} noValidate autoComplete="off">
-      <TextField
-        autoComplete="email"
-        className={classes.textField}
-        id="email"
-        label="Email"
-        type="email"
-        value={props.email}
-        onChange={e => props.setEmail(e.target.value)}
-        margin="normal"
-        error={props.error.email}
-        helperText={props.error.email ? 'Please enter your email.' : ' '}
-      />
-      <TextField
-        autoComplete="current-password"
-        className={classes.textField}
-        id="password"
-        label="Password"
-        type="password"
-        value={props.password}
-        onChange={e => props.setPassword(e.target.value)}
-        margin="normal"
-        error={props.error.password}
-        helperText={props.error.password ? 'Please enter your password.' : ' '}
-      />
-      <TextField
-        className={classes.textField}
-        id="courseId"
-        label="Course ID"
-        type="number"
-        value={props.courseId}
-        onChange={e => props.setCourseId(e.target.value)}
-        margin="normal"
-        error={props.error.courseId}
-        helperText={props.error.courseId ? 'Please enter a course ID.' : ' '}
-      />
+      <Grid container direction="row" justify="space-between" spacing={2}>
+        <Grid item xs>
+          <TextField
+            autoComplete="email"
+            className={classes.textField}
+            id="email"
+            label="Email"
+            type="email"
+            value={props.email}
+            onChange={e => props.setEmail(e.target.value)}
+            margin="normal"
+            error={props.error.email}
+            helperText={props.error.email ? 'Please enter your email.' : ' '}
+          />
+        </Grid>
+        <Grid item xs>
+          <TextField
+            autoComplete="current-password"
+            className={classes.textField}
+            id="password"
+            label="Password"
+            type="password"
+            value={props.password}
+            onChange={e => props.setPassword(e.target.value)}
+            margin="normal"
+            error={props.error.password}
+            helperText={
+              props.error.password ? 'Please enter your password.' : ' '
+            }
+          />
+        </Grid>
+        <Grid item xs>
+          <TextField
+            className={classes.textField}
+            id="courseId"
+            label="Course ID"
+            type="number"
+            value={props.courseId}
+            onChange={e => props.setCourseId(e.target.value)}
+            margin="normal"
+            error={props.error.courseId}
+            helperText={
+              props.error.courseId ? 'Please enter a course ID.' : ' '
+            }
+          />
+        </Grid>
+      </Grid>
     </form>
   );
 };
