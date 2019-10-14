d3101ca8d11bb887beb8d1199f33ce50e2a50b9e
petercolella <peter.joseph.colella@gmail.com>
2019-10-13 23:29:55 -0400

    adds Collapse component to CurrentCourse component which transitions based on courseLoaded prop

--- client/src/components/CurrentCourse/index.js
+++ client/src/components/CurrentCourse/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Typography } from '@material-ui/core';
+import { Collapse, Paper, Typography } from '@material-ui/core';
 import InactiveStudents from '../InactiveStudents';
 import FilteredAssignments from '../FilteredAssignments';
 
@@ -28,28 +28,30 @@ const CurrentCourse = props => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.paper} elevation={4}>
-      <Typography
-        className={classes.headingContainer}
-        variant="h6"
-        gutterBottom>
-        Current Course ID: {props.currentCourseId}
-      </Typography>
-      <div className={classes.buttonContainer}>
-        <InactiveStudents
-          assignments={props.assignments}
-          courseDbId={props.courseDbId}
-          inactiveStudents={props.inactiveStudents}
-          loadData={props.loadData}
-        />
-        <FilteredAssignments
-          assignments={props.assignments}
-          courseDbId={props.courseDbId}
-          filteredAssignments={props.filteredAssignments}
-          loadData={props.loadData}
-        />
-      </div>
-    </Paper>
+    <Collapse in={props.courseLoaded} timeout={'auto'}>
+      <Paper className={classes.paper} elevation={4}>
+        <Typography
+          className={classes.headingContainer}
+          variant="h6"
+          gutterBottom>
+          Current Course ID: {props.currentCourseId}
+        </Typography>
+        <div className={classes.buttonContainer}>
+          <InactiveStudents
+            assignments={props.assignments}
+            courseDbId={props.courseDbId}
+            inactiveStudents={props.inactiveStudents}
+            loadData={props.loadData}
+          />
+          <FilteredAssignments
+            assignments={props.assignments}
+            courseDbId={props.courseDbId}
+            filteredAssignments={props.filteredAssignments}
+            loadData={props.loadData}
+          />
+        </div>
+      </Paper>
+    </Collapse>
   );
 };
 
