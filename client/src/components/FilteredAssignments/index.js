5ca662ed543e07e68be7d14935702e758717a149
petercolella <peter.joseph.colella@gmail.com>
2019-10-16 20:02:23 -0400

    adds sort method to arrays in FilteredAssignments/InactiveStudents components

--- client/src/components/FilteredAssignments/index.js
+++ client/src/components/FilteredAssignments/index.js
@@ -54,7 +54,7 @@ const FilteredAssignments = props => {
           []
         )
       )
-    );
+    ).sort();
     setTitleArr(newTitleArr);
   };
 
--- client/src/components/InactiveStudents/index.js
+++ client/src/components/InactiveStudents/index.js
@@ -54,7 +54,7 @@ const InactiveStudents = props => {
           []
         )
       )
-    );
+    ).sort();
     setNameArr(newNameArr);
   };
 
