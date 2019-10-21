71ba0eec3ff1bd25653b5d05b4245a65c9f0438d
petercolella <peter.joseph.colella@gmail.com>
2019-10-20 23:59:04 -0400

    updates text content and styling of Welcome dialog

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -9,12 +9,26 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4)
+  },
+  contentText: {
+    // paddingLeft: '3.75rem',
+    // paddingRight: '3.75rem'
+  },
   dialog: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
     minHeight: `calc(100vh - ${theme.spacing(12)}px)`,
     textAlign: 'center'
+  },
+  title: {
+    padding: theme.spacing(4)
   }
 }));
 
@@ -28,16 +42,29 @@ const Welcome = () => {
   };
 
   return (
-    <Dialog fullWidth maxWidth="xl" onClose={handleClose} open={open}>
+    <Dialog
+      fullWidth
+      maxWidth="xl"
+      onClose={handleClose}
+      open={open}
+      scroll={'body'}>
       <div className={classes.dialog}>
-        <DialogTitle disableTypography>
+        <DialogTitle className={classes.title} disableTypography>
           <Typography align="center" variant="h2">
             Homework Status
           </Typography>
         </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Welcome! This app uses the Bootcamp Spot grades API.
+        <DialogContent className={classes.content}>
+          <DialogContentText className={classes.contentText} variant="h5">
+            Welcome!
+          </DialogContentText>
+          <DialogContentText className={classes.contentText}>
+            This app uses the Bootcamp Spot Grades API. Enter your email address
+            and password, along with the course ID of your cohort.
+          </DialogContentText>
+          <DialogContentText className={classes.contentText}>
+            If you would like a demo, use Email: <em>test@test.com</em>,
+            Password: <em>test</em>, and Course ID: <em>1111</em>.
           </DialogContentText>
         </DialogContent>
         <DialogActions>
