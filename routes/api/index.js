f924249598f6c100ac13730ae809b932a758707e
petercolella <peter.joseph.colella@gmail.com>
2020-01-04 22:04:42 -0500

    deletes user controller and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const assignmentRoutes = require('./assignments');
 const courseRoutes = require('./courses');
-const userRoutes = require('./users');
 
 router.use('/assignments', assignmentRoutes);
 router.use('/courses', courseRoutes);
-router.use('/users', userRoutes);
 
 module.exports = router;
